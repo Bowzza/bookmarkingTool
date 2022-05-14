84af8cce43010999bd0d0ded90eb79a3de05ec1c
Markus <markus.mayer1@stud.fh-campuswien.ac.at>
2022-05-14 15:00:07 +0200

    added exception if url is null

--- src/main/java/at/ac/fhcampuswien/bookmarkingtool/BookmarkingTool.java
+++ src/main/java/at/ac/fhcampuswien/bookmarkingtool/BookmarkingTool.java
@@ -10,6 +10,7 @@ public class BookmarkingTool {
     private List<Url> urlList = new ArrayList<>();
 
     public void bookmarkUrl(Url url) {
+        if (url == null) throw new IllegalArgumentException("Url must not be null");
         urlList.add(url);
     }
 
