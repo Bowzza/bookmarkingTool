629716d4552b8061b3f8a383050622aeeecfc5bc
flo19 <flo.mayerhofe99@gmail.com>
2022-05-15 15:25:20 +0200

    added test method for adding a null keyword to a URL

--- src/test/java/at/ac/fhcampuswien/bookmarkingtool/BookmarkingToolTagUrlWithKeywordTest.java
+++ src/test/java/at/ac/fhcampuswien/bookmarkingtool/BookmarkingToolTagUrlWithKeywordTest.java
@@ -4,6 +4,7 @@ import at.ac.fhcampuswien.bookmarkingtool.model.Url;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BookmarkingToolTagUrlWithKeywordTest {
 
@@ -14,12 +15,33 @@ public class BookmarkingToolTagUrlWithKeywordTest {
         Url url = new Url("https://www.fh-campuswien.ac.at/");
         String expectedResult = "fh campus";
         String result;
+        String keyword = "fh campus";
         BookmarkingTool bookmarkingTool = new BookmarkingTool();
 
         // Act
         bookmarkingTool.bookmarkUrl(url);
-        bookmarkingTool.getUrlList().get(0).setKeyword("fh campus");
+        bookmarkingTool.getUrlList().get(0).setKeyword(keyword);
         result = bookmarkingTool.getUrlList().get(0).getKeyword();
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void ensureAddingNullKeywordToUrl() {
+
+        // Arrange
+        Url url = new Url("https://www.fh-campuswien.ac.at/");
+        String expectedResult = "Keyword must not be null";
+        String result;
+        String keyword = null;
+        Exception exception;
+        BookmarkingTool bookmarkingTool = new BookmarkingTool();
+
+        // Act
+        bookmarkingTool.bookmarkUrl(url);
+        exception = assertThrows(IllegalArgumentException.class, () -> bookmarkingTool.getUrlList().get(0).setKeyword(keyword));
+        result = exception.getMessage();
+
         // Assert
         assertEquals(expectedResult, result);
     }
