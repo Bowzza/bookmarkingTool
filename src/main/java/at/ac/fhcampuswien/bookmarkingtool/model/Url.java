bcd31f692ca21d2ae5b4179073e3a508f1d880d3
Markus <markus.mayer1@stud.fh-campuswien.ac.at>
2022-05-15 15:06:06 +0200

    added test method for adding a valid keyword to a URL

--- src/main/java/at/ac/fhcampuswien/bookmarkingtool/model/Url.java
+++ src/main/java/at/ac/fhcampuswien/bookmarkingtool/model/Url.java
@@ -2,6 +2,7 @@ package at.ac.fhcampuswien.bookmarkingtool.model;
 
 public class Url {
     private String url;
+    private String keyword;
 
     public Url(String url) {
         this.url = url;
@@ -9,5 +10,13 @@ public class Url {
 
     public String getUrl() {
         return url;
+    }
+
+    public String getKeyword() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setKeyword(String keyword) {
+        throw new UnsupportedOperationException();
     }
 }
--- src/test/java/at/ac/fhcampuswien/bookmarkingtool/BookmarkingToolTestValidUrl.java
+++ src/test/java/at/ac/fhcampuswien/bookmarkingtool/BookmarkingToolTestValidUrl.java
@@ -10,7 +10,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BookmarkingToolTestValidUrl {
 
-
     @ParameterizedTest
     @CsvSource({"https://www.fh-campuswien.ac.at/, https://www.fh-campuswien.ac.at/", "http://www.fh-campuswien.ac.at/, http://www.fh-campuswien.ac.at/"})
     public void ensureBookmarkingValidUrl(String input, String expectedResult) {
@@ -47,6 +46,7 @@ public class BookmarkingToolTestValidUrl {
     @ParameterizedTest
     @CsvSource({"://www.fh-campuswien.ac.at/, no protocol: ://www.fh-campuswien.ac.at/", "htt://www.fh-campuswien.ac.at/, unknown protocol: htt", "httpswww.fh-campuswien.ac.at/, no protocol: httpswww.fh-campuswien.ac.at/,", "https/:/www.fh-campuswien.ac.at/, no protocol: https/:/www.fh-campuswien.ac.at/", "https://www.fh-campuswi^n.ac.at/, Illegal character in authority at index 8: https://www.fh-campuswi^n.ac.at/", "' ', 'no protocol:  '", "https://www.fh-cam puswien.ac.at/, Illegal character in authority at index 8: https://www.fh-cam puswien.ac.at/", "'', 'no protocol: '"})
     public void ensureBookmarkingUrlWithIllegalCharactersNoProtocolUnknownProtocol(String input, String expectedResult) {
+
         // Arrange
         Url url = new Url(input);
         String result;
@@ -56,6 +56,23 @@ public class BookmarkingToolTestValidUrl {
         // Act
         result = exception.getMessage();
 
+        // Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void ensureAddingValidKeywordToUrl() {
+
+        // Arrange
+        Url url = new Url("https://www.fh-campuswien.ac.at/");
+        String expectedResult = "fh campus";
+        String result;
+        BookmarkingTool bookmarkingTool = new BookmarkingTool();
+
+        // Act
+        bookmarkingTool.bookmarkUrl(url);
+        bookmarkingTool.getUrlList().get(0).setKeyword("fh campus");
+        result = bookmarkingTool.getUrlList().get(0).getKeyword();
         // Assert
         assertEquals(expectedResult, result);
     }
